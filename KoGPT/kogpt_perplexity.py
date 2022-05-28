4b8a44732e1ef0956fd5e29da8d06d7e3042cbca
JINHEE-KANG <genie3192@gmail.com>
2022-05-28 11:46:21 +0000

    remove perplexity code

--- KoGPT/kogpt_perplexity.py
+++
@@ -1,8 +0,0 @@
-from kogpt_inference import generate_text
-
-# validation - perplexity
-    test_file_paths = os.listdir(test_dir)
-
-    for i,test_file in enumerate(test_file_paths):
-        test_file_path = os.path.join(test_dir,test_file)
-        test_dataset = load_dataset(test_file_path, tokenizer)
