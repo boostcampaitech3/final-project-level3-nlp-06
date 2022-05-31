42f0e0b55928f633a5840791d5b5fdd6e3c3e54d
chacha <km04278@naver.com>
2022-05-31 10:46:09 +0000

    update textdataset

--- KoGPT/kogpt_trainer.py
+++ KoGPT/kogpt_trainer.py
@@ -3,53 +3,20 @@ import torch
 import torch.nn.functional as F
 from typing import NoReturn
 from transformers import (
-    TextDataset,
     DataCollatorForLanguageModeling,
     AutoModelForCausalLM,
     Trainer,
     TrainingArguments,
-    AutoTokenizer
+    AutoTokenizer,
+    GPT2LMHeadModel,
+    PreTrainedTokenizerFast
 )
 
-def perplexity(output, target):
-    output = np.array(output)
-    target = np.array(target)
-    if output.shape[0] <= target.shape[0]:
-        zero_output = np.zeros_like(target)
-        zero_output[:output.shape[0]] = output
-        output = zero_output
-    else:
-        zero_output = np.zeros_like(output)
-        zero_output[:target.shape[0]] = target
-        target = zero_output
-
-    target = torch.FloatTensor(target)
-    output = torch.FloatTensor(output)
-    loss = F.cross_entropy(output,target)
-    return torch.exp(loss)
-
-def compute_metrics(output,target):
-    ppl = perplexity(output,target)
-    return ppl
-
-def load_dataset(file_path, tokenizer, block_size = 128):
-    dataset = TextDataset(
-        tokenizer = tokenizer,
-        file_path = file_path,
-        block_size = block_size,
-    )
-    return dataset
- 
-def load_data_collator(tokenizer, mlm = False):
-    data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer, 
-        mlm=mlm,
-    )
-    return data_collator
+from util import TextDataset, perplexity, load_dataset, load_data_collator
 
 # Train
 def train(
-    train_file_path, 
+    dir_path, 
     model_name, 
     output_dir,
     overwrite_output_dir,
@@ -58,45 +25,42 @@ def train(
     save_steps
     ):
     # https://huggingface.co/skt/ko-gpt-trinity-1.2B-v0.5
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(model_name, 
+                    bos_token='</s>', eos_token='</s>', unk_token='<unk>',
+                    pad_token='<pad>', mask_token='<mask>')
 
     print('load dataset...')
-    train_dataset = load_dataset(train_file_path, tokenizer)
+    train_dataset = load_dataset(dir_path, tokenizer)
     data_collator = load_data_collator(tokenizer)
-    print(f'{train_file_path} loaded!')
+    print(f'{dir_path} loaded!')
     
     tokenizer.save_pretrained(output_dir, legacy_format=False)
     print(f'Tokenizer save_pretrained. {output_dir}')
     
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name, 
-        pad_token_id=tokenizer.pad_token_id,
-        bos_token_id=tokenizer.bos_token_id,
-        eos_token_id=tokenizer.eos_token_id,
-        torch_dtype='auto', low_cpu_mem_usage=True
-        ).to(device='cuda', non_blocking=True)
-
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    
     model.save_pretrained(output_dir)
     print(f'Model save_pretrained. {output_dir}')
     
     training_args = TrainingArguments(
-            output_dir=output_dir,
-            overwrite_output_dir=overwrite_output_dir,
-            per_device_train_batch_size=per_device_train_batch_size,
-            num_train_epochs=num_train_epochs,
-            #fp16=True,
-        )
+        output_dir=output_dir,
+        overwrite_output_dir=overwrite_output_dir,
+        per_device_train_batch_size=per_device_train_batch_size,
+        num_train_epochs=num_train_epochs,
+        #fp16=True,
+    )
 
     print(training_args)
     trainer = Trainer(
-            model=model,
-            args=training_args,
-            data_collator=data_collator,
-            train_dataset=train_dataset,
-            compute_metrics = compute_metrics,
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
     )
         
     trainer.train()
+    print("Training Done!!")
+
     trainer.save_model()
     print(f'model saved.')
 
@@ -105,18 +69,18 @@ def train(
 def main():
     torch.cuda.empty_cache()
     # 모델명, 데이터 위치, 모델 저장위치, Training arguments를 정의합니다.
-    train_file_path = './final_final_train.txt'
+    dir_path = 'datasets'
     model_name = 'skt/ko-gpt-trinity-1.2B-v0.5'
-    output_dir = './output'
+    output_dir = 'models'
     overwrite_output_dir = False
     per_device_train_batch_size = 4 # batch 사이즈 조절 시 oom이 발생할 수 있습니다...
-    num_train_epochs = 20
+    num_train_epochs = 10
     save_steps = 500
     
     print(f'Modle is {model_name}')
     
     train(
-        train_file_path=train_file_path,
+        dir_path=dir_path,
         model_name=model_name,
         output_dir=output_dir,
         overwrite_output_dir=overwrite_output_dir,
