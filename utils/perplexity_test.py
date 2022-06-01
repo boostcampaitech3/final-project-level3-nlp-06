f1f1563eac2c01117d8d36f35d429c8727f51cf0
chacha <km04278@naver.com>
2022-06-01 13:35:00 +0000

    add perpexity example

---
+++ utils/perplexity_test.py
@@ -0,0 +1,38 @@
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from datasets import load_dataset
+import torch
+from tqdm import tqdm
+
+
+def main():
+    device = "cuda"
+    model_id = "skt/kogpt2-base-v2"
+    model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+    tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
+
+    test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+    encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt")
+
+    max_length = model.config.n_positions
+    stride = 256
+
+    nlls = []
+    for i in tqdm(range(0, encodings.input_ids.size(1), stride)):
+        begin_loc = max(i + stride - max_length, 0)
+        end_loc = min(i + stride, encodings.input_ids.size(1))
+        trg_len = end_loc - i  # may be different from stride on last loop
+        input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+        target_ids = input_ids.clone()
+        target_ids[:, :-trg_len] = -100
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+            neg_log_likelihood = outputs[0] * trg_len
+
+        nlls.append(neg_log_likelihood)
+
+    ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
+    print(ppl)
+
+if __name__ == "__main__":
+    main()
